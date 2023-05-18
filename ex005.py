8fe8fa62b1efa0148330d7932d7112e910b7dcaf
rodrigoviegas <roussadoviegasrodrigo@gmail.com>
2023-05-18 10:20:56 +0100

    atualizações

--- ex005.py
+++ ex005.py
@@ -1,2 +1,4 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
 num = int(input('Insira um número: '))
 print(f'O seu antecessor é {num-1} e o seu sucessor é {num+1}')
