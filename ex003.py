f3c5b7b54c346b6cb6ba43ad4bd31ec6512f8203
rodrigoviegas <roussadoviegasrodrigo@gmail.com>
2023-05-18 10:00:15 +0100

    ex003

--- ex003.py
+++ ex003.py
@@ -1,1 +1,5 @@
-# ex003 - 
+# ex003 - ler dois números e somá-los
+
+num1 = int(input('Insira um número: '))
+num2 = int(input('Insira outro número: '))
+print('O valor da soma dos dois números é {}'.format(num1+num2))
