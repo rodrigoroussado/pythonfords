396a5b00481bc5b2a70dbd4803904dea77cafb73
rodrigoviegas <roussadoviegasrodrigo@gmail.com>
2023-05-18 10:35:20 +0100

    ex006 e ex007

--- ex006.py
+++ ex006.py
@@ -1,1 +1,4 @@
-# 
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = float(input('Insira um número: '))
+print(f'O dobro de {n} é {n*2:.3}, o triplo é {n*3:.3} e a sua raiz quadrada é {n**(1/2):.3}')
---
+++ ex007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+nota1 = float(input('Insira a nota do primeiro teste: '))
+nota2 = float(input('Insira a nota do segundo teste: '))
+
+print(f'A média das notas é: {(nota1+nota2)/2}')
